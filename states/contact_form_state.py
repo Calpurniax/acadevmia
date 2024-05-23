d2814b5d0b2db26c92ef0e65fd7666fcdcbbb148
Calpurniax <sofia.gracia@gmail.com>
2024-05-23 13:25:57 +0200

    add contact logic

--- states/contact_form_state.py
+++ states/contact_form_state.py
@@ -1,8 +1,11 @@
 import reflex as rx
 
+from db.firebase import add_comment
+
 class contactFormState(rx.State):
     contact_data: dict = {}
 
     def handle_submit(self, contact_data: dict):
         """Handle the form submit."""
         self.contact_data = contact_data
+        add_comment(contact_data)
