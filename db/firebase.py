be18a40bed69c3d977365e656de39cfc73fe56e4
Calpurniax <sofia.gracia@gmail.com>
2024-05-23 13:37:46 +0200

    change contact

--- db/firebase.py
+++ db/firebase.py
@@ -1,3 +1,5 @@
+import reflex as rx
+
 import firebase_admin
 from firebase_admin import firestore, credentials
 
@@ -6,20 +8,24 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 comments_ref = db.collection('comentarios')
 
-# def add_comment(data):    
-#    doc_ref= comments_ref.add(data)
-#    response = search_comment(doc_ref)
+def add_comment(data):  
+    doc_ref= comments_ref.add(data)
+   
+    response = search_comment(doc_ref)
+    rx.console.log(response)
 #    return response
+
    
 
-# def search_comment(id):
-#     doc = comments_ref.document(id).get()
-#     # doc_ref = db.collection("cities").document("SF")
-#     # doc = doc_ref.get()
-#     if doc.exists:
-#         return True
-#     else:
-#         return False
+def search_comment(id):
+    doc = comments_ref.document(id).get()
+    rx.console.log(doc)
+    # doc_ref = db.collection("cities").document("SF")
+    # doc = doc_ref.get()
+    if doc.exists:
+        return True
+    else:
+        return False
 
-def add_comment(data):    
-    comments_ref.document(str(data['contact_email'])).set(data)
+# def add_comment(data):    
+#     comments_ref.document(str(data['contact_email'])).set(data)
